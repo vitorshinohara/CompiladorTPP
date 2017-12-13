b7af1c9b309d3edfaf626156a3f9d9346f842ed6
Vitor Yudi Shinohara <vitor.ysh@gmail.com>
2017-12-12 23:49:11 -0200

    get Args Names added

--- src/implementacao/GenCodigo.py
+++ src/implementacao/GenCodigo.py
@@ -13,14 +13,13 @@ class GenCode(object):
 		self.variaveis = []
 		self.variaveisGlobais = []
 		self.escopo = "global"
+		self.funcs = []
 		self.iterar(self.arvore)
 		self.funcllvm = ''
-
 		arquivo = open('vars.ll', 'w')
 		arquivo.write(str(self.module))
 		arquivo.close()
 		print(self.module)
-
 
 	def iterar(self, node):
 		if node != None:
@@ -33,35 +32,72 @@ class GenCode(object):
 			for son in node.child:
 				self.iterar(son)
 
-
 	def declaracao_funcao(self,node): # Declaração de uma função
+		# Verifica por argumentos da função
 
 		self.variaveis = self.variaveisGlobais # Zera a tabela de variaveis da função, deixa só variraveis globais
-
+		args = []
 		if len(node.child) > 1: # Função com retorno
 			nome = node.child[1].value
 			self.escopo = nome # Escopo recebe o nome da função
+			if node.child[1].child[0] is not None:
+				# Passa o nó da lista de argumentos
+				args = self.getArgsFunc(node.child[1].child[0], [])
+				nomes = self.getArgsFuncName(node.child[1].child[0], [])
+
 			tipo = node.child[0].type # Tipo da função <inteiro\float>
 
 			if tipo == 'inteiro':
 				# Cria um retorno de tipo inteiro
-				t_func = ir.FunctionType(ir.IntType(32), ())
+				t_func = ir.FunctionType(ir.IntType(32), (args))
 			else:
 				# Cria um retorno de tipo float
-				t_func = ir.FunctionType(ir.FloatType(), ())
+				t_func = ir.FunctionType(ir.FloatType(), (args))
 		
 		else:
 			nome = node.child[0].value # Função void
+			self.escopo = nome
+			# Verifica se existe argumentos na função
+			if node.child[0].child[0] is not None:
+				# Obtem os argumentos
+				args = self.getArgsFunc(node.child[0].child[0], [])
+				nomes = self.getArgsFuncName(node.child[1].child[0], [])
+
 			# Cria um retorno de tipo void
-			t_func = ir.FunctionType(ir.VoidType(), ())
+			t_func = ir.FunctionType(ir.VoidType(), (args))
 
 		# Cria a função
 		func = ir.Function(self.module, t_func, nome)
+		# Adiciona a função na lista de funções llvm
+		self.funcs.append(func)
+		# Obtem os argumentos da função
+		argumentos_llvm = func.args
+		
+		if len(argumentos_llvm) > 0:
+			print argumentos_llvm[0].type
+
+		# Define qual função o código vai utilizar na iteração do corpo
 		self.funcllvm = func
 		# Cria o bloco de entrada e saida da função
 		entryBlock = func.append_basic_block('entry' + nome)
 		# Adiciona o bloco de entrada da função
 		builder = ir.IRBuilder(entryBlock)
+		# Percorre a lista de parâmetros do tipo args llvm
+		for x in range(0,len(argumentos_llvm)):
+			# Se o argumento for do tipo inteiro
+			
+			if str(argumentos_llvm[x].type) == 'i32':
+				#  Aloca a variável inteira com o nome do argumento referente ao mesmo
+				var = builder.alloca(ir.IntType(32), str(nomes[x]))
+				self.variaveis.append(var)
+
+			# Se o argumento for do tipo flutuante
+			elif str(argumentos_llvm[x].type) == 'float':
+				print nomes[x]
+				#  Aloca a variável float com o nome do argumento referente ao mesmo
+				var = builder.alloca(ir.FloatType(), str(nomes[x]))
+				self.variaveis.append(var)
+		
 		# Itera sobre o corpo da função
 		self.iterar_corpo(node, builder)
 		# Adiciona o bloco de saida
@@ -71,6 +107,30 @@ class GenCode(object):
 		# Adiciona bloco de saída no final da função
 		builder.position_at_end(endBasicBlock)
 
+	def getArgsFunc(self, node, args):
+		# Recursão para os filhos
+		if len(node.child) > 1:
+			for son in node.child:
+				args = self.getArgsFunc(son, args)
+				
+		else:
+			tipo = node.child[0].child[0].type
+			if tipo == 'inteiro':
+				args.append(ir.IntType(32))
+			elif tipo == 'flutuante':
+				args.append(ir.FloatType())
+		return args
+
+	def getArgsFuncName(self, node, args):
+		aux = []
+		if len(node.child) > 1:
+			for son in node.child:
+				args = self.getArgsFuncName(son, args)
+				
+		else:
+			if node.type == 'lista_parametros':
+				args.append(node.child[0].value)
+				return args
 
 	def variavelGlobal(self, node): # Define variáveis globais
 		tipo = node.child[0].type   # Armazena o tipo da var
@@ -95,7 +155,6 @@ class GenCode(object):
 		# Adiciona var global na lista de var globais
 		self.variaveisGlobais.append(globalVar) # Adiciona var global na lista de var globais
 
-
 	def iterar_corpo(self, node, builder): # Itera o corpo de uma função
 		if node is not None:
 			# Se o nó for do tipo declaracao_variaveis
@@ -118,10 +177,31 @@ class GenCode(object):
 				# Adiciona as operações para se realizar um loop
 				self.repita(node, builder)
 
+			if node.type == 'chamada_funcao':
+				self.chamada_funcao(node, builder)
+
 			for son in node.child:
 				self.iterar_corpo(son, builder)
 
+	def chamada_funcao(self, node, builder):
+		if node.child[0] is None:
+			# Faz a chamada da função sem argumentos
+			builder.call(self.searchFunc(node.value), (), node.value)
+			# IRBuilder.call(fn, args, name='')
+			#				 ^	  ^		^
+			#	function name     args  nome
 
+		else:
+			args_str = []
+			args_str = self.getArgsFuncName(node.child[0].child[0])
+			print args_str
+			args_llvm = []
+			
+			for arg in args_str:
+				# Transforma os argumentos str em llvm
+				args_llvm.append(builder.load(self.searchVarTable(arg), ""))
+			# Faz a chamada da função, passa os argumentos e define o nome	
+			builder.call(self.searchFunc(node.value), args_llvm, node.value)
 
 	def repita(self, node, builder):
 		# Declara o bloco do predicado (verificação do laço)
@@ -164,7 +244,6 @@ class GenCode(object):
 			builder.select(cmp, body, endloop)
 
 		builder.position_at_end(endloop)
-			
 
 	def declaracao_variavel(self, node, builder):
 		tipo = node.child[0].type
@@ -199,7 +278,6 @@ class GenCode(object):
 				var.align = 4
 				self.variaveis.append(var)
 
-
 	def atribuicao(self, node, builder):
 		arr = []
 		var = node.child[0].value
@@ -222,8 +300,7 @@ class GenCode(object):
 
 		if len(arr) == 2:								# variavel = 1
 			x = self.searchVarTable(arr[0])				# ^          ^
-			builder.load(x, "")							# x         var
-			arr.pop(0)
+			arr.pop(0)									# x         var
 			var = self.searchVarTable(arr[0])
 			var = self.genCodeTypes(var, builder)
 			builder.store(var, x)
@@ -236,8 +313,6 @@ class GenCode(object):
 			temp = self.resolverArray(builder, arr)
 			
 			builder.store(temp, x) # Armazena o resultado da operação no x
-		
-
 
 	def resolverArray(self, builder, arr):
 		# Inicializa variável temporária
@@ -345,7 +420,6 @@ class GenCode(object):
 			# Fim do if
 			builder.position_at_end(merge)		
 
-
 	def searchVarTable(self, var): # Procura a variável na tabela ou faz conversões
 		for x in self.variaveis:
 			if x.name == str(var):
@@ -383,12 +457,15 @@ class GenCode(object):
 			except Exception as e:
 				pass
 
+	def searchFunc(self, nome):
+		for function in self.funcs:
+			if function.name == nome:
+				return function
+
 	def getType(self, var):
 		for x in self.variaveis:
 			if var == x.name:
 				return x.type
-		
-
 
 	def expressao(self, node, arr):
 		if node.child[0].type == 'expressao_simples':
@@ -416,7 +493,6 @@ class GenCode(object):
 			arr  = self.operador_multiplicacao(node.child[1], arr)
 			arr = self.expressao_unaria(node.child[2], arr)
 			return arr
-
 
 	def expressao_multiplicativa(self, node,arr):
 		if len(node.child) == 1:
@@ -454,9 +530,13 @@ class GenCode(object):
 
 			if node.child[0].type == 'var':
 				arr.append(node.child[0].value)
+				return arr
 			elif node.child[0].type == 'numero':
 				arr.append(node.child[0].value)	
-			return arr
+				return arr
+			# elif node.child[0].type == 'chamada_funcao':
+			# 	self.chamada_funcao(node.child[0])
+			# 	return arr
 
 if __name__ == '__main__':
 	try:
